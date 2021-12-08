81eb0755b791887a8e4f4321a1da44077e952749
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-12-08 17:29:26 +0100

    feat: init version of lua processor with protobuf

--- pkg/processor/luaprocessor/examples/script.lua
+++ pkg/processor/luaprocessor/examples/script.lua
@@ -1,39 +1,25 @@
-function dump(o, indent)
-  if type(o) == 'table' then
-    local s = ''
-    for k,v in pairs(o) do
-      if type(k) ~= 'number' then k = '"' .. k .. '"' end
-      s = s .. indent .. k .. ': '
-      if type(v) == 'table' then
-        local st = dump(v, indent .. ' ')
-        if string.len(st) > 0 then
-          s = s .. '\n' .. st
-        end
-      else
-        s = s .. dump(v, indent) .. '\n'
-      end
-    end
-    return s
-  else
-    return tostring(o)
-  end
-end
+local pb = require "pb"
+local protoc = require "protoc"
 
-function process(data)
+function process(bytesData)
+    assert(protoc.new():loadfile "opentelemetry/proto/metrics/v1/metrics.proto")
+    local data = assert(pb.decode(".opentelemetry.proto.metrics.v1.MetricsData", bytesData))
+    -- print(require "serpent".block(data))
+
     if type(data) == 'table' then
-      resourceMetrics = data["resourceMetrics"]
+      resourceMetrics = data["resource_metrics"]
       for kResourceMetrics, vResourceMetrics in pairs(resourceMetrics) do
-        libraryMetrics = vResourceMetrics["libraryMetrics"]
+        libraryMetrics = vResourceMetrics["instrumentation_library_metrics"]
         for kLibraryMetrics, vLibraryMetrics in pairs(libraryMetrics) do
           metrics = vLibraryMetrics["metrics"]
           for kMetrics, vMetrics in pairs(metrics) do
-            dataPoints = vMetrics["sum"]["dataPoints"]
+            dataPoints = vMetrics["sum"]["data_points"]
             for kDataPoints, vDataPoints in pairs(dataPoints) do
               dataPoint = vDataPoints
               -- change startTimestamp
-              dataPoint["startTimestamp"] = 0
+              dataPoint["start_time_unix_nano"] = 0
               -- change value
-              dataPoint["value"] = 789
+              dataPoint["as_double"] = 789
               -- add attribute
               dataPoint["attributes"]["lua"] = "true"
             end
@@ -41,6 +27,11 @@ function process(data)
         end
       end
     end
-    return data
+
+    encoded = pb.encode(".opentelemetry.proto.metrics.v1.MetricsData", data)
+    return pb.tohex(encoded)
 end
 
+-- function process(data)
+--   return pb.tohex(data)
+-- end
--- pkg/processor/luaprocessor/processor.go
+++ pkg/processor/luaprocessor/processor.go
@@ -2,11 +2,14 @@ package luaprocessor
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
-
 	"go.opentelemetry.io/collector/model/pdata"
 
+	"strings"
+
 	glua "github.com/RyouZhang/go-lua"
+	"go.opentelemetry.io/collector/model/otlp"
 )
 
 type luaProcessor struct {
@@ -23,21 +26,39 @@ func newLuaProcessor(cfg *Config) *luaProcessor {
 
 // ProcessMetrics processes metrics
 func (lp *luaProcessor) ProcessMetrics(ctx context.Context, md pdata.Metrics) (pdata.Metrics, error) {
-	// TODO: add processor logic here
 	fmt.Println("***Hello from Lua metrics processor***")
 
-	converter := NewMetricsLuaConverter(md)
-	params := converter.ConvertToLua()
-
-	fmt.Println("Lua processor, script: ", lp.script, ", function: ", lp.function)
-	res, err := glua.NewAction().WithScriptPath(lp.script).WithEntrypoint(lp.function).AddParam(params).Execute(context.Background())
+	marshaler := otlp.NewProtobufMetricsMarshaler()
+	data, err := marshaler.MarshalMetrics(md)
 	if err != nil {
 		fmt.Println(err)
 		return md, err
 	}
-	fmt.Println("*** lua script returned", res)
 
-	return converter.ConvertFromLua(res)
+	fmt.Println("Lua processor, script: ", lp.script, ", function: ", lp.function)
+	var res interface{}
+	res, err = glua.NewAction().WithScriptPath(lp.script).WithEntrypoint(lp.function).AddParam(data).Execute(context.Background())
+	if err != nil {
+		fmt.Println("lua err", err)
+		return md, err
+	}
+
+	outString := res.(string)
+	splittedString := strings.Split(outString, " ")
+	dataBin, err := hex.DecodeString(strings.Join(splittedString, ""))
+	if err != nil {
+		fmt.Println("decodeString ", err)
+		return md, err
+	}
+
+	var newMetrics pdata.Metrics
+	unmarshaler := otlp.NewProtobufMetricsUnmarshaler()
+	newMetrics, err = unmarshaler.UnmarshalMetrics(dataBin)
+	if err != nil {
+		fmt.Println("marshaller ", err)
+		return md, err
+	}
+	return newMetrics, err
 }
 
 // ProcessTraces processes traces
